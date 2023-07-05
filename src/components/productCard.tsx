3353b4774489d5da3070c4f441fc8efbe7970807
Carlos Eduardo <carlospcavalari@gmail.com>
2023-07-04 21:22:16 -0300

    to pull

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Image from "next/image";
 import carro from "../assets/car.webp";
+import { useContext } from "react";
+import { UserContext } from "@/contexts/contexts";
 
 interface iProduct {
   id: string;
@@ -17,13 +19,14 @@ interface iProduct {
   coverImage: string;
 }
 const ProductCard = ({ announcement }: { announcement: iProduct }) => {
+  const { router }: any = useContext(UserContext);
   const sellValueNumber = parseInt(announcement.sellPrice);
   const fipeValueNumber = parseInt(announcement.fipePrice);
   const isSellProfitable =
     sellValueNumber + fipeValueNumber * 0.05 <= fipeValueNumber;
   return (
     <>
-      <div className="min-w-[80%] max-w-[312px] max-sm:text-sm relative">
+      <div className="min-w-[80%] max-w-[312px] max-sm:text-sm relative cursor-pointer">
         <p
           className={
             isSellProfitable
@@ -33,7 +36,7 @@ const ProductCard = ({ announcement }: { announcement: iProduct }) => {
         >
           {isSellProfitable && "$"}
         </p>
-        <div className="hover:border-brand-100 hover:border-2 border-2 border-transparent">
+        <div onClick={() => router.push(`details/`)} className="hover:border-brand-100 hover:border-2 border-2 border-transparent">
           <Image className="bg-gray-500" src={carro} alt="coverImage" />
         </div>
         <div className="flex flex-col gap-2 p-3">
